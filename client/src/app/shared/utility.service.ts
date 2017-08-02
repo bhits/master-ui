439924691119128174f44b3649e5fb4c3d35a4fd
Teng Luo <Teng.Luo@feisystems.com>
2017-08-02 11:54:29 -0400

    Fix issue that if coming request override default port

--- client/src/app/shared/utility.service.ts
+++ client/src/app/shared/utility.service.ts
@@ -22,6 +22,6 @@ export class UtilityService {
     let protocol:string = window.location.protocol;
     let host:string = window.location.host;
     let port:string = window.location.port;
-    return protocol.concat("//").concat(host).concat( port? ":".concat(port).concat("/"): "/");
+    return protocol.concat("//").concat(host).concat( (!port)? ":".concat(port).concat("/"): "/");
   }
 }
